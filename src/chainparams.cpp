28c8c6dfc6d8aae7907122aae37af4cd5f0f49c4
Jenova7 <38404798+Jenova7@users.noreply.github.com>
2019-02-12 13:52:01 +0100

    Update Bip44 Hexa

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -201,7 +201,7 @@ public:
         base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 161);
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
-        base58Prefixes[EXT_COIN_TYPE] = boost::assign::list_of(0x80)(0x12)(0x00)(0x00).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[EXT_COIN_TYPE] = boost::assign::list_of(0x80)(0x00)(0x00)(0xf9).convert_to_container<std::vector<unsigned char> >();
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
