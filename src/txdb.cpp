253d554ef69d904917ac68822ce73723e5dc7c60
Jenova7 <38404798+Jenova7@users.noreply.github.com>
2020-02-10 12:23:48 +0100

    Revert "fix PoW genesis check"
    This reverts commit 605c6b647d94e3c4f901c0a2fecf1fa07c1b6925.

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -262,7 +262,7 @@ bool CBlockTreeDB::LoadBlockIndexGuts()
                 pindexNew->nStakeTime = diskindex.nStakeTime;
                 pindexNew->hashProofOfStake = diskindex.hashProofOfStake;
 
-                if (pindexNew->GetBlockHash() != Params().HashGenesisBlock() && pindexNew->IsProofOfWork() && !CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits))
+                if (pindexNew->IsProofOfWork() && !CheckProofOfWork(pindexNew->GetBlockHash(), pindexNew->nBits))
                     return error("LoadBlockIndex() : CheckProofOfWork failed: %s", pindexNew->ToString());
 
                 //populate accumulator checksum map in memory
